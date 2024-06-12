99b69504df2e812050566c3864776d622a87d979
designsoo <kheesoo10@gmail.com>
2024-06-13 03:39:52 +0900

    chore: storybook 배경 dark 테마 고정

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,7 @@
-import type { Preview } from "@storybook/react";
-import "@fontsource/plus-jakarta-sans/400.css";
-import "@fontsource/plus-jakarta-sans/600.css";
-import '../src/index.css'
+import type { Preview } from '@storybook/react';
+import '@fontsource/plus-jakarta-sans/400.css';
+import '@fontsource/plus-jakarta-sans/600.css';
+import '../src/index.css';
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +10,16 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#0A0A0A',
+        },
+      ],
     },
   },
 };
