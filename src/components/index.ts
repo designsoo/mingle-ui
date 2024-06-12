561b9d46d23fe6195a6a38c181affa7e918bf6a1
designsoo <kheesoo10@gmail.com>
2024-06-13 06:09:33 +0900

    feat: inputField 컴포넌트 export

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ import '@/index.css';
 export { default as Label } from '@/components/Label.tsx';
 export { default as Input } from '@/components/Input.tsx';
 export { default as ErrorMessage } from '@/components/ErrorMessage.tsx';
+export { default as InputField } from '@/components/InputField.tsx';
