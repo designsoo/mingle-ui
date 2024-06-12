d28b5f0b89c2b6b3816cd6b1479fe41d97b16e2e
designsoo <kheesoo10@gmail.com>
2024-06-13 04:08:42 +0900

    feat: 눈 아이콘 svg 파일 추가 및 상수 파일 생성

---
+++ src/constants/image.ts
@@ -0,0 +1,12 @@
+export const SVGS = {
+  eye: {
+    on: {
+      url: '/ic-eye-on.svg',
+      alt: 'show icon',
+    },
+    off: {
+      url: '/ic-eye-off.svg',
+      alt: 'off icon',
+    },
+  },
+};
