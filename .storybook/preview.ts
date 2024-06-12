aeab6ef0092c3708e08d4289cdd33479d0d36c4e
designsoo <kheesoo10@gmail.com>
2024-06-12 19:24:48 +0900

    chore: jakarta-sans 폰트 설치 및 적용

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from "@storybook/react";
+import "@fontsource/plus-jakarta-sans/400.css";
+import "@fontsource/plus-jakarta-sans/600.css";
 import '../src/index.css'
 
 const preview: Preview = {
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import "@fontsource/plus-jakarta-sans/400.css";
+import "@fontsource/plus-jakarta-sans/600.css";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
