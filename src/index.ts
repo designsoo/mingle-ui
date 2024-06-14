5d97915a18b3e4ecdf8ae84ccf39f6a430bf776c
designsoo <kheesoo10@gmail.com>
2024-06-15 01:15:14 +0900

    feat: 구현된 전체 컴포넌트 index.ts 파일에 export

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,8 @@ export { default as Label } from '@/components/Label.tsx';
 export { default as Input } from '@/components/Input.tsx';
 export { default as ErrorMessage } from '@/components/ErrorMessage.tsx';
 export { default as InputField } from '@/components/InputField.tsx';
+export { default as BadgeEmoji } from '@/components/BadgeEmoji.tsx';
+export { default as Category } from '@/components/Category.tsx';
+export { default as IconButton } from '@/components/IconButton.tsx';
+export { default as PrimaryButton } from '@/components/PrimaryButton.tsx';
+export { default as TabList } from '@/components/TabList.tsx';
