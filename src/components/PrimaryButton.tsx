ede8ce66e45b94887f926947bd549e36910daa30
designsoo <kheesoo10@gmail.com>
2024-06-15 02:13:08 +0900

    refactor: 중복코드 제거 및 고정값있는 props 옵셔널하게 받도록 재설정

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -4,35 +4,27 @@ type ButtonSize = 'sm' | 'md' | 'lg';
 interface PrimaryButtonProps {
   children: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
-  size: ButtonSize;
-  variant: ButtonTheme;
-  disabled: boolean;
+  size?: ButtonSize;
+  variant?: ButtonTheme;
+  disabled?: boolean;
 }
 
-const primary = 'button-fill-primary';
-const secondary = 'button-fill-secondary';
-const stroke = 'button-stroke';
-
 const buttonTheme: Record<ButtonTheme, string> = {
-  primary,
-  secondary,
-  stroke,
+  primary: 'button-fill-primary',
+  secondary: 'button-fill-secondary',
+  stroke: 'button-stroke',
 };
 
-const sm = 'h-9 px-2';
-const md = 'h-11 px-3';
-const lg = 'h-12 w-full text-bold-16';
-
 const buttonSize: Record<ButtonSize, string> = {
-  sm,
-  md,
-  lg,
+  sm: 'h-9 px-2',
+  md: 'h-11 px-3',
+  lg: 'h-12 w-full text-bold-16',
 };
 
 const PrimaryButton = ({
   children,
   onClick,
-  size = 'lg',
+  size = 'md',
   variant = 'primary',
   disabled = false,
 }: PrimaryButtonProps) => {
