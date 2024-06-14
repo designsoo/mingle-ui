c3222ee18bc0e368966f2e171ae4db6db828a3c2
designsoo <kheesoo10@gmail.com>
2024-06-14 13:25:14 +0900

    refactor: Cloudflare CDN 이미지 적용

--- src/constants/image.ts
+++ src/constants/image.ts
@@ -1,11 +1,14 @@
+const CLOUDFLARE_URL = 'https://imagedelivery.net/gHftqKLNBQ-NOWw8XS0zUw/';
+const ICON_SIZE_20 = 'width=20,height=20';
+
 export const SVGS = {
   eye: {
     on: {
-      url: '/ic-eye-on.svg',
+      url: `${CLOUDFLARE_URL}6c54eafe-3e97-4c82-2ac5-f6d44ca95100/${ICON_SIZE_20}`,
       alt: 'show icon',
     },
     off: {
-      url: '/ic-eye-off.svg',
+      url: `${CLOUDFLARE_URL}159fda3c-e69a-4c20-44cf-3d477efda000/${ICON_SIZE_20}`,
       alt: 'off icon',
     },
   },
