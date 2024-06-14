503af8fb87ec16d93bd43252aeb38d73b8b76c64
designsoo <kheesoo10@gmail.com>
2024-06-14 17:27:46 +0900

    feat: BadgeEmoji 컴포넌트 storybook 연동

---
+++ stories/BadgeEmoji.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import BadgeEmoji from '../src/components/BadgeEmoji';
+
+const meta = {
+  title: 'Badge/BadgeEmoji',
+  component: BadgeEmoji,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+
+  argTypes: {
+    emoji: {
+      control: 'text',
+      description: '선택된 emoji',
+    },
+    count: {
+      control: 'text',
+      description: 'emoji 총 개수',
+    },
+  },
+} satisfies Meta<typeof BadgeEmoji>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    emoji: '🥳',
+    count: '10',
+  },
+};
