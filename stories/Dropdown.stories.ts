1c6ba7cc835c9cc1a7063efb5e14b294d916fc0b
designsoo <kheesoo10@gmail.com>
2024-06-16 04:50:30 +0900

    feat: Dropdown 컴포넌트 storybook 연동

---
+++ stories/Dropdown.stories.ts
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Dropdown from '../src/components/Dropdown';
+
+const meta = {
+  title: 'Dropdown/Dropdown',
+  component: Dropdown,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+
+  argTypes: {
+    selectList: {
+      control: 'object',
+      description: '드롭다운에 표시될 항목들의 목록',
+    },
+    size: {
+      control: 'select',
+      options: ['md', 'sm'],
+      description: '드롭다운의 크기',
+    },
+  },
+} satisfies Meta<typeof Dropdown>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const MediumSize: Story = {
+  args: {
+    selectList: [
+      { id: 0, value: 'Latest' },
+      { id: 1, value: 'Oldest' },
+    ],
+    size: 'md',
+    onclick: (id) => console.log(id),
+  },
+};
+
+export const SmallSize: Story = {
+  args: {
+    selectList: [
+      { id: 0, value: 'Latest' },
+      { id: 1, value: 'Oldest' },
+    ],
+    size: 'sm',
+    onclick: (id) => console.log(id),
+  },
+};
