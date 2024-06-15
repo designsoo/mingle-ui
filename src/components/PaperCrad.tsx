1d1b9dbe3a852ba2dba7eea8dcf15920238b834b
designsoo <kheesoo10@gmail.com>
2024-06-16 02:06:05 +0900

    feat: Paper card 컴포넌트 구현

---
+++ src/components/PaperCrad.tsx
@@ -0,0 +1,44 @@
+import Category from '@/components/Category';
+
+interface PaperCradProps {
+  category: string;
+  fromName: string;
+  description: string;
+  backgroundImage: string;
+}
+
+const PaperCrad = ({
+  category,
+  fromName,
+  description,
+  backgroundImage,
+}: PaperCradProps) => {
+  function createMarkup(html: string) {
+    return { __html: html };
+  }
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+      }}
+      className='flex h-[400px] w-full flex-col justify-end overflow-hidden rounded-2xl bg-neutral-800 p-4'
+    >
+      <div className='relative h-[240px] overflow-hidden rounded-xl p-4 pt-11 color-background-opacity-black-70'>
+        <div className='absolute right-4 top-4'>
+          <Category>{category}</Category>
+        </div>
+        <h2 className='pb-3 text-bold-18 text-neutral-200'>{fromName}</h2>
+        <p
+          dangerouslySetInnerHTML={createMarkup(description)}
+          className='scroll-bar-sm h-[140px] overflow-y-scroll pr-2 text-base-16 text-neutral-400'
+        ></p>
+      </div>
+    </div>
+  );
+};
+
+export default PaperCrad;
