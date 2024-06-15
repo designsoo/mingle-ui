58719f81a23d5407f2ecfa376c6f1b709ffd11de
designsoo <kheesoo10@gmail.com>
2024-06-15 21:50:16 +0900

    refactor: Input 컴포넌트 react-hook-form 적용(useFormContext/register), props 일부 변경

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,20 +1,29 @@
 import { InputHTMLAttributes, useState } from 'react';
 import { PASSWORD_SHOW_MODE } from '@/constants';
+import { useFormContext } from 'react-hook-form';
+import ErrorMessage from '@/components/ErrorMessage';
 
 interface InputProps {
-  id: string;
   name: string;
   type: string;
   placeholder: string;
+  errorMessage: string;
+  isRequired?: boolean;
 }
 
 const Input = ({
-  id,
   name,
   type,
   placeholder,
+  errorMessage,
+  isRequired = false,
   ...rest
 }: InputProps & InputHTMLAttributes<HTMLInputElement>) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
   const [isFocused, setIsFocused] = useState(false);
   const borderColor = isFocused ? 'border-yellow-300' : 'border-neutral-700';
 
@@ -28,25 +37,36 @@ const Input = ({
   const onInputBlur = () => setIsFocused(false);
 
   return (
-    <div
-      onFocus={onInputFocus}
-      onBlur={onInputBlur}
-      className={`base-transition flex h-11 w-full items-center justify-between rounded-lg border px-4 ${borderColor}`}
-    >
-      <input
-        className='h-full w-full bg-transparent text-base-16 text-neutral-200 outline-none placeholder:text-neutral-700'
-        id={id}
-        name={name}
-        type={type === 'password' ? inputType : type}
-        placeholder={placeholder}
-        {...rest}
-      />
-      {type === 'password' && (
-        <button onClick={onEyeButtonClick} className='pl-3'>
-          <img src={iconEye} alt={showMode} />
-        </button>
-      )}
-    </div>
+    <>
+      <div
+        onFocus={onInputFocus}
+        onBlur={onInputBlur}
+        className={`base-transition flex h-11 w-full items-center justify-between rounded-lg border px-4 ${borderColor}`}
+      >
+        <input
+          className='h-full w-full bg-transparent text-base-16 text-neutral-200 outline-none placeholder:text-neutral-700'
+          {...register(name, { required: isRequired && errorMessage })}
+          id={name}
+          type={type === 'password' ? inputType : type}
+          placeholder={placeholder}
+          {...rest}
+        />
+        {type === 'password' && (
+          <button
+            type='button'
+            role='switch'
+            aria-label={showMode}
+            aria-checked={passwordShowMode}
+            onClick={onEyeButtonClick}
+            className='pl-3'
+          >
+            <img src={iconEye} alt={showMode} />
+          </button>
+        )}
+      </div>
+
+      {errors[name] && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </>
   );
 };
 
