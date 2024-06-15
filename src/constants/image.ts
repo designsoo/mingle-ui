2c16d4da6c001dc618c83dd7646bebf218e2f5b4
designsoo <kheesoo10@gmail.com>
2024-06-16 04:49:56 +0900

    feat: arrow SVG 아이콘 추가

--- src/constants/image.ts
+++ src/constants/image.ts
@@ -1,5 +1,6 @@
 const CLOUDFLARE_URL = 'https://imagedelivery.net/gHftqKLNBQ-NOWw8XS0zUw/';
 const ICON_SIZE_20 = 'width=20,height=20';
+const ICON_SIZE_16 = 'width=16,height=16';
 
 export const SVGS = {
   eye: {
@@ -15,5 +16,15 @@ export const SVGS = {
   empty: {
     url: `${CLOUDFLARE_URL}f1d8565c-836e-4fc1-42fd-1b1364da7b00/${ICON_SIZE_20}`,
     alt: 'empty icon',
+  },
+  arrow: {
+    up: {
+      url: `${CLOUDFLARE_URL}10c20999-b186-4950-8625-bd0c1acf8900/${ICON_SIZE_16}`,
+      alt: 'arrow up',
+    },
+    down: {
+      url: `${CLOUDFLARE_URL}35b2aade-a28e-4e15-77ba-485774a91e00/${ICON_SIZE_16}`,
+      alt: 'arrow down',
+    },
   },
 };
