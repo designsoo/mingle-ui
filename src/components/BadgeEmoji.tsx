cd820a3f88653492c9c27c51a1bbbefd62df7a7b
designsoo <kheesoo10@gmail.com>
2024-06-23 00:42:21 +0900

    refactor: badge emoji count prop 타입 number로 변경

--- src/components/BadgeEmoji.tsx
+++ src/components/BadgeEmoji.tsx
@@ -1,13 +1,13 @@
 interface BadgeEmojiProps {
   emoji: string;
-  count: string;
+  count: number;
 }
 
 const BadgeEmoji = ({ emoji, count }: BadgeEmojiProps) => {
   return (
-    <div className='color-background-opacity-white-10 h-8 gap-1 rounded-full px-3 flexbox-row'>
+    <div className='h-8 gap-1 rounded-full px-3 flexbox-row color-background-opacity-white-10'>
       <span className='text-base-14'>{emoji}</span>
-      <span className='color-text-primary text-base-14'>{count}</span>
+      <span className='text-base-14 color-text-primary'>{count}</span>
     </div>
   );
 };
