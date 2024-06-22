0dcbe906c44c1595548da97f3d79bac1fa454a23
designsoo <kheesoo10@gmail.com>
2024-06-23 00:40:50 +0900

    refactor: useTogglePopup 및 shadow 스타일 적용

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { SVGS } from '@/constants';
+import useTogglePopup from '@/hooks/useTogglePopup';
 
 const {
   arrow: {
@@ -30,8 +31,9 @@ const Dropdown = <T,>({
   onClick,
   size = 'md',
 }: DropdownProps<T>) => {
-  const [isOpen, setIsOpen] = useState(false);
   const [selectedItem, setSeletedItem] = useState(selectList[0]?.value);
+  const { isOpen, popupRef, buttonRef, togglePopup, setIsOpen } =
+    useTogglePopup();
 
   const handleSelectItemClick = (id: T, value: string) => {
     setSeletedItem(value);
@@ -39,43 +41,41 @@ const Dropdown = <T,>({
     setIsOpen(false);
   };
 
-  const toggleClick = () => {
-    setIsOpen((prev) => !prev);
-  };
-
   return (
     <div className='flex w-full flex-col gap-2'>
       <div
-        className={`base-transition flex items-center justify-between rounded-lg border border-neutral-700 hover:border-neutral-200 focus:border-yellow-300 ${DropdownTheme[size]} ${isOpen && 'border-yellow-300 hover:border-yellow-300'}`}
+        className={`base-transition flex items-center justify-between rounded-lg border border-neutral-700 bg-neutral-950 hover:border-neutral-200 focus:border-yellow-300 ${DropdownTheme[size]} ${isOpen && 'border-yellow-300 hover:border-yellow-300'}`}
       >
         <span className='min-w-16 grow text-base-14 text-neutral-200'>
           {selectedItem}
         </span>
 
-        <button className='pl-3' type='button'>
+        <button className='pl-3' type='button' ref={buttonRef}>
           <img
             src={url}
             alt={alt}
             width={16}
-            onClick={toggleClick}
+            onClick={togglePopup}
             className={`base-transition transform ${isOpen ? 'rotate-180' : '-rotate-0'}`}
           />
         </button>
       </div>
       {isOpen && (
-        <ul className='flex w-full flex-col rounded-lg border border-neutral-700 bg-neutral-950 p-1 *:text-base-14 *:text-neutral-500'>
-          {selectList.map((item) => (
-            <li key={`dropdown-key-${item.id}`}>
-              <button
-                type='button'
-                className='base-transition h-9 w-full rounded-md px-2 py-[6px] text-start hover:text-neutral-200 hover:color-background-opacity-white-5'
-                onClick={() => handleSelectItemClick(item.id, item.value)}
-              >
-                {item.value}
-              </button>
-            </li>
-          ))}
-        </ul>
+        <div ref={popupRef}>
+          <ul className='dropdown-shadow flex w-full flex-col rounded-lg border border-neutral-700 bg-neutral-950 p-1 *:text-base-14 *:text-neutral-500'>
+            {selectList.map((item) => (
+              <li key={`dropdown-key-${item.id}`}>
+                <button
+                  type='button'
+                  className='base-transition h-9 w-full rounded-md px-2 py-[6px] text-start hover:text-neutral-200 hover:color-background-opacity-white-5'
+                  onClick={() => handleSelectItemClick(item.id, item.value)}
+                >
+                  {item.value}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       )}
     </div>
   );
