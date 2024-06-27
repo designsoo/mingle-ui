583982852ef50a2b5659380ef9953ae35872437f
designsoo <kheesoo10@gmail.com>
2024-06-27 22:29:29 +0900

    refactor: tailwind prettier 적용

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -4,8 +4,8 @@ interface CategoryProps {
 
 const Category = ({ children }: CategoryProps) => {
   return (
-    <div className='color-border-category h-7 rounded border px-2 flexbox-row'>
-      <span className='color-text-primary inline-block min-w-12 text-center text-base-12'>
+    <div className='h-7 rounded border px-2 flexbox-row color-border-category'>
+      <span className='inline-block min-w-12 text-center text-base-12 color-text-primary'>
         {children}
       </span>
     </div>
