a8812d347d064ee20f77ee9393fabfa6fed64711
designsoo <kheesoo10@gmail.com>
2024-07-03 06:21:34 +0900

    feat: Storybook 커스터 마이징

---
+++ .storybook/theme.ts
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Mingle Storybook',
+  brandUrl: 'https://github.com/designsoo/mingle-ui',
+  brandImage:
+    'https://imagedelivery.net/gHftqKLNBQ-NOWw8XS0zUw/b571045b-4bb3-4222-36d5-8557bcf7f300/width=72,height=24',
+  brandTarget: '_self',
+});
