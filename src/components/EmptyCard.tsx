a57de425f5507cda942ed57e782cacc7b119b85d
designsoo <kheesoo10@gmail.com>
2024-07-08 20:09:25 +0900

    refactor: Empty Card 메시지 색상 수정

--- src/components/EmptyCard.tsx
+++ src/components/EmptyCard.tsx
@@ -8,9 +8,7 @@ const EmptyCard = () => {
     <div className='flex h-[400px] w-full overflow-hidden rounded-2xl border-2 border-dashed border-neutral-800 p-4 flexbox-row'>
       <div className='gap-2 flexbox-column'>
         <img src={url} alt={alt} width={48} height={48} />
-        <span className='text-bold-16 text-neutral-500'>
-          Mingle Paper Not Found
-        </span>
+        <span className='text-bold-16 text-neutral-400'>Mingle Paper Not Found</span>
       </div>
     </div>
   );
