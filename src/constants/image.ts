71ee5fb6c123717cf6577a3c8523498a581e7812
designsoo <kheesoo10@gmail.com>
2024-07-15 05:50:14 +0900

    refactor: 내부 SVG 이미지 URL 적용

--- src/constants/image.ts
+++ src/constants/image.ts
@@ -1,34 +1,37 @@
-const CLOUDFLARE_URL = 'https://imagedelivery.net/gHftqKLNBQ-NOWw8XS0zUw';
-const ICON_SIZE_20 = 'width=20,height=20';
-const ICON_SIZE_16 = 'width=16,height=16';
+import arrow_down from '/src/assets/ic-arrow-down.svg';
+import arrow_up from '/src/assets/ic-arrow-up.svg';
+import eye_on from '/src/assets/ic-eye-on.svg';
+import eye_off from '/src/assets/ic-eye-off.svg';
+import empty from '/src/assets/ic-empty.svg';
+import find_card from '/src/assets/ic-find-card.svg';
 
 export const SVGS = {
   eye: {
     on: {
-      url: `${CLOUDFLARE_URL}/6c54eafe-3e97-4c82-2ac5-f6d44ca95100/${ICON_SIZE_20}`,
+      url: eye_on,
       alt: 'show password',
     },
     off: {
-      url: `${CLOUDFLARE_URL}/159fda3c-e69a-4c20-44cf-3d477efda000/${ICON_SIZE_20}`,
+      url: eye_off,
       alt: 'hide password',
     },
   },
   empty: {
-    url: `${CLOUDFLARE_URL}/f1d8565c-836e-4fc1-42fd-1b1364da7b00/${ICON_SIZE_20}`,
+    url: empty,
     alt: 'empty icon',
   },
   arrow: {
     up: {
-      url: `${CLOUDFLARE_URL}/10c20999-b186-4950-8625-bd0c1acf8900/${ICON_SIZE_16}`,
+      url: arrow_up,
       alt: 'arrow up',
     },
     down: {
-      url: `${CLOUDFLARE_URL}/35b2aade-a28e-4e15-77ba-485774a91e00/${ICON_SIZE_16}`,
+      url: arrow_down,
       alt: 'arrow down',
     },
   },
   find: {
-    url: `${CLOUDFLARE_URL}/bfd078e7-fede-46f3-23e0-4cbbe4f48500/${ICON_SIZE_20}`,
+    url: find_card,
     alt: 'find paper icon',
   },
 };
