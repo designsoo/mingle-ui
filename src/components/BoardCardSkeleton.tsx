290f2c2d84cc379fc69690f47402e61d2d68d679
designsoo <kheesoo10@gmail.com>
2024-07-15 06:39:47 +0900

    refactor: board card 스켈레톤 너비 지정할 수 있도록 prop 추가

--- src/components/BoardCardSkeleton.tsx
+++ src/components/BoardCardSkeleton.tsx
@@ -1,6 +1,13 @@
-const BoardCardSkeleton = () => {
+interface BoardCardSkeletonProps {
+  cardWidth?: string;
+}
+
+const BoardCardSkeleton = ({ cardWidth = '100%' }: BoardCardSkeletonProps) => {
   return (
-    <article className='h-[320px] w-[265px] overflow-hidden rounded-2xl bg-neutral-900 p-4'>
+    <article
+      style={{ width: `${cardWidth}` }}
+      className='h-[320px] w-[265px] overflow-hidden rounded-2xl bg-neutral-900 p-4'
+    >
       <div className='flex h-full w-full flex-col justify-end'>
         <div className='flex h-[136px] w-full flex-col justify-between rounded-xl bg-neutral-800 p-3'>
           <div className='h-6 w-32 animate-pulse rounded-md bg-neutral-700'></div>
